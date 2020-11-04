771ab2def5e96efb17b9898df22846b63ffe61bf
Pinois <>
2020-11-04 09:31:07 +0300

    first commit

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserContext from './userContext';
+
+function Header() {
+	return (
+		<header>
+			<UserContext.Consumer>{username => <p>Welcome, {username}!</p>}</UserContext.Consumer>
+		</header>
+	);
+}
+
+export default Header;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import UserContext from './userContext';
+
+ReactDOM.render(
+	<UserContext.Provider value={'bob123'}>
+		<App />
+	</UserContext.Provider>,
+	document.getElementById('root')
+);
